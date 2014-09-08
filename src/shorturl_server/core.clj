6832b36d13b4dcb1d57d251200d68fcd45509018
Davide <davide.bertolotto@gmail.com>
2014-09-08 23:43:09 +0200

    initial commit

---
+++ src/shorturl_server/core.clj
@@ -0,0 +1,68 @@
+(ns shorturl-server.core
+  (:use [compojure.core :only [GET PUT POST defroutes]])
+  (:require [compojure.handler :as handler :only [api site]]
+            [compojure.route :as route :only [not-found]]
+            [ring.util.response :as ring-response :only [redirect]]
+            [ring.adapter.jetty :as ring-jetty :only [run-jetty]]
+            ))
+
+(def ^:private counter (atom 0))
+
+(def ^:private mappings (ref {}))
+
+(defn url-for
+  "Retrieve the URL for a certain ID"
+  [id]
+  (@mappings id))
+
+(defn shorten!
+  "Stores the given URL under a new unique identifier, or the given
+  identifier if provided. Returns the identifier as a sting. Modifies the
+  global mapping accordingly."
+  ([url]
+     (let [id (swap! counter inc)
+           id (Long/toString id 36)]
+       (or (shorten! url id)
+           (recur url))) )
+  ([url id]
+     (dosync
+      (when-not (url-for id)
+        (alter mappings assoc id url)
+        id))))
+
+;;(shorten! "http://a.website")
+;;(shorten! "http://a.website" "a-short-id")
+;;(shorten! "http://another.website" "a-short-id")
+;;@mappings
+
+(defn retain
+  "Store the url"
+  [& [url id :as args]]
+  (if-let [id (apply shorten! args)]
+    {:status 201
+     :headers {"Location" id}
+     :body (format "URL %s assigned the short identifier %s\n" url id)}
+    {:status 409
+     :body (format "Short URL %s is already taken.\n" id)}))
+
+(defn redirect
+  "Redirect to the actual url, if it exists"
+  [id]
+  (if-let [url (url-for id)]
+    (ring-response/redirect url)
+    {:status 404
+     :body (format "No such short URL: %s\n" id)}))
+
+(defroutes app*
+  (GET "/" [] "Welcome!")
+  (PUT "/:id" [url id] (retain url id))
+  (POST "/" [url] (retain url))
+  (GET "/:id" [id] (redirect id))
+  (GET "/list/" [] (concat (interpose "\n" (keys @mappings)) "\n"))
+  (route/not-found "Sorry, there's nothing here.\n"))
+
+(def app
+  (handler/site app*))
+
+;;(def server (ring-jetty/run-jetty #'app {:port 8080 :join? false}))
+;;(.stop server)
---
+++ test/shorturl_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns shorturl-server.core-test
+  (:require [clojure.test :refer :all]
+            [shorturl-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
